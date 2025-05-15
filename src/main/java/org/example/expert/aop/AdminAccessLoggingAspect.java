3c8bf3ecac48b3cba999cad7793b86d4ad4e0e3a
bella <yiseuljeong0415@gmail.com>
2025-05-16 00:35:39 +0900

    feat: AOP 포인트컷 수정하여 changeUserRole 메소드 호출 전 로깅 추가

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -6,25 +6,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+/**
+ * 관리자 API 실행 전 요청 로그를 기록하는 AOP 클래스
+ */
 @Slf4j
 @Aspect
 @Component
-@RequiredArgsConstructor
 public class AdminAccessLoggingAspect {
 
-    private final HttpServletRequest request;
-
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
-    public void logAfterChangeUserRole(JoinPoint joinPoint) {
-        String userId = String.valueOf(request.getAttribute("userId"));
-        String requestUrl = request.getRequestURI();
-        LocalDateTime requestTime = LocalDateTime.now();
-
-        log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
-                userId, requestTime, requestUrl, joinPoint.getSignature().getName());
+    /**
+     * UserAdminController의 changeUserRole 메소드가 실행되기 전에 로그를 찍음
+     *
+     * @param joinPoint 실행되는 메소드의 정보를 담고 있음
+     */
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
+    public void logBeforeChangingUserRole(JoinPoint joinPoint) {
+        log.info("[ADMIN LOG] changeUserRole() 실행 전 호출됨: {}", joinPoint.getSignature());
     }
 }
